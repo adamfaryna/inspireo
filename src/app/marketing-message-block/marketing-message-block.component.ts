296cf3c638f7c8793182e582c48569143ed1e52e
Adam Faryna <adamfaryna@gmail.com>
2016-12-06 01:54:41 +0000

    Added marketing-message-block

--- src/app/marketing-message-block/marketing-message-block.component.ts
+++ src/app/marketing-message-block/marketing-message-block.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-marketing-message-block',
   templateUrl: './marketing-message-block.component.html',
-  styleUrls: ['./marketing-message-block.component.css']
+  styleUrls: ['./marketing-message-block.component.css'],
+  host: { class: 'content-block' }
 })
 export class MarketingMessageBlockComponent implements OnInit {
 
