af20efb0a38aad594e8d611bcaa742c780a47996
Adam Faryna <adamfaryna@gmail.com>
2016-12-06 01:55:00 +0000

    Added tel-input-form.

--- src/app/tel-input-form/tel-input-form.component.ts
+++ src/app/tel-input-form/tel-input-form.component.ts
@@ -3,11 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-tel-input-form',
   templateUrl: './tel-input-form.component.html',
-  styleUrls: ['./tel-input-form.component.css']
+  styleUrls: ['./tel-input-form.component.css'],
+  host: { class: 'content-block' }
 })
 export class TelInputFormComponent implements OnInit {
 
   constructor() { }
+
+  model: {tel: string} = {
+    tel: ''
+  };
+
+  submit() {
+
+  }
 
   ngOnInit() {
   }
